27143cfff592ce4325f6f89697d9484dee173c84
vrooo <kotoliszka@gmail.com>
2021-03-22 14:20:38 +0100

    Add project files.

---
+++ UnderwaterGLDemo/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	const int width = 1600;
+	const int height = 900;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, 0);
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "Test", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+
+	glViewport(0, 0, width, height);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
